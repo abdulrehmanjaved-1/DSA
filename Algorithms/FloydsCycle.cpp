68fdd9a6e1efdb2e88e418eaeac26618d3c8219f
abdulrehmanjaved-1 <abdulrehmanjaveds12@gmail.com>
2023-10-23 10:17:12 +0500

    day230

---
+++ Algorithms/FloydsCycle.cpp
@@ -0,0 +1,16 @@
+//TODO: find the cycle present or not in linkedList (ie: this is circular or not),also find the first node of the cycle,
+//then remove the cycle ...simply by pointing that to NULL.
+//--> hints: Point of interaction by slow and fast approach... and for finding out the starting of loop use point slow to start ag8in method and 
+//continue POI and slow with same pace now and find where they meet as : A+B=x*C;
+
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    
+};
+int main()
+{
+    
+    return 0;
+}
